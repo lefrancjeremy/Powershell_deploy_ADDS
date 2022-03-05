62a9a869236c51912b1065226f0b2ebd36268f21
lefrancjeremy <jeremy.lefranc@epitech.eu>
2022-03-05 01:55:44 +0100

    Add files via upload

---
+++ Powershell_deploy_ADDS.ps1
@@ -0,0 +1,18 @@
+#
+# Script Windows PowerShell pour le déploiement d’AD DS
+#
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "lcs.local" `
+-DomainNetbiosName "LCS" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
